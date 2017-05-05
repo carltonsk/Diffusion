0035c6aa6e04075b385651eab689438cc63cef3b
carltonsk <carltonkenworthy@yahoo.com>
2017-05-05 07:20:56 -0500

    Side Codes and Results

---
+++ f.m
@@ -0,0 +1,7 @@
+function F = f(x)
+ax = 0;
+ay = 0;
+bx = 2*pi;
+by = 2*pi;
+F = x*(x-ax)^2;
+end
---
+++ g.m
@@ -0,0 +1,7 @@
+function G = g(x)
+ax = 0;
+bx = 2*pi;
+ay = 0;
+by = 2*pi;
+G = ((x-ax)^2)*cos(x);
+end
